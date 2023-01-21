e12ea443364c186f7e2b2fcd49d02092d9777eeb
Csaba79-coder <csabavadasz79@gmail.com>
2023-01-21 16:19:22 +0100

    refactor repository regarding @Where annotation and soft delete (means set Availability to DELETED in DB)

--- src/main/java/com/csaba79coder/bookliquibase/domain/book/persistence/BookRepository.java
+++ src/main/java/com/csaba79coder/bookliquibase/domain/book/persistence/BookRepository.java
@@ -11,5 +11,4 @@ import java.util.UUID;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findBookById(UUID id);
-    void deleteBookById(UUID id);
 }
